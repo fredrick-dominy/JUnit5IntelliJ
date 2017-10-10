ab6c3d24a31fb743871a0410805ca339637d13c8
Fredrick Dominy <fdominy@r2integrated.com>
2017-10-09 17:18:54 -0700

    Added age range class

---
+++ src/main/java/com/example/AgeRangeCompatabilityCalculator.java
@@ -0,0 +1,37 @@
+package com.example;
+
+/*
+* Given an integer (1 <= n <= 100) representing a person's age,
+* return their minimum and maximum age range.
+*
+* min = age/2 + 7
+* max = (age-7) * 2
+*
+* This equation doesn't work when the age <= 14, so use this equation instead:
+* min = age - 0.10 * age
+* max = age + 0.10 * age
+* You should floor all your answers so that an integer is given
+* instead of a float (which doesn't represent age).
+* Return your answer in the form [min]-[max]
+*
+* Examples:
+* age = 27   =>   20-40
+* age = 5    =>   4-5
+* age = 17   =>   15-20
+*
+*/
+public class AgeRangeCompatabilityCalculator {
+    public static String datingRange(int age) {
+        int min;
+        int max;
+
+        if(age < 14) {
+            min = (int)(age - (0.10 * age));
+            max = (int)(age+(0.10 * age));
+        } else {
+            min = (int)((age/2)+7);
+            max = (int)((age-7)*2);
+        }
+        return Integer.toString(min) +  "-" + Integer.toString(max);
+    }
+}
---
+++ src/test/java/com/example/AgeRangeCompatabilityCalculatorTests.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AgeRangeCompatabilityCalculatorTests {
+    @Test
+    public void fifteenTwenty() {
+        assertEquals("15-20",AgeRangeCompatabilityCalculator.datingRange(17));
+    }
+    @Test
+    public void tsss() {
+        assertEquals("27-66",AgeRangeCompatabilityCalculator.datingRange(40));
+    }
+    @Test
+    public void frtnsxtn() {
+        assertEquals("14-16",AgeRangeCompatabilityCalculator.datingRange(15));
+    }
+    @Test
+    public void twfrftysx() {
+        assertEquals("24-56",AgeRangeCompatabilityCalculator.datingRange(35));
+    }
+    @Test
+    public void nnllvn() {
+        assertEquals("9-11",AgeRangeCompatabilityCalculator.datingRange(10));
+    }
+}
