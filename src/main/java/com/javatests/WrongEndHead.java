5238ec2ef8029374d744fcd327dbd02c4111441c
Fredrick Dominy <fdominy@r2integrated.com>
2017-10-11 20:47:17 -0700

    Added new exercise WrongEndHead

--- src/main/java/com/javatests/WrongEndHead.java
+++ src/main/java/com/javatests/WrongEndHead.java
@@ -1,10 +1,14 @@
 package com.javatests;
 
 
-import org.apache.maven.shared.utils.StringUtils;
+
 
 public class WrongEndHead {
     public static Object[] fixTheMeerkat(String[] strings) {
-
+        String[] tempArr = new String[strings.length];
+        for (int i = 0; i < strings.length; i ++) {
+            tempArr[strings.length-1-i] = strings[i];
+        }
+        return tempArr;
     }
 }
